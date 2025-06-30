ece1154a1b1ffbe9191f76d1cbc55fc70ef4d197
Shrafi Shehab <svshuvo696@gmail.com>
2025-07-01 01:54:09 +0600

    date function complete

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -8,27 +8,33 @@ import {useState} from "react";
 import SearchBox from "./SearchBox/SearchBox.js";
 
 const Event = () => {
-    const axiosPublic = useAxiosPublic();
-
+    // search
     const [searchValue, setSearchValue] = useState("");
     const placeholders = [
         "Search for events...",
     ];
+    // date 
+    const [dateValue, setDateValue] = useState("");
 
+    const axiosPublic = useAxiosPublic();
     const { data:events = [] } = useQuery({
-        queryKey: ["events", searchValue],
+        queryKey: ["events", searchValue, dateValue],
         queryFn: async () => {
-            const res = await axiosPublic.get(`/events/all-events?search=${searchValue}`);
+            const res = await axiosPublic.get(`/events/all-events?search=${searchValue}&date=${dateValue}`);
             return (res.data) as IEventData[];
         }
     });
 
+    const handleDateFilter = (e) => {
+        setDateValue(e.target.value)
+    }
+    console.log(dateValue);
 
     return (
         <Container>
             <div>
                 <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} placeholders={placeholders}/>
-
+                <input type="datetime-local" value={dateValue} onChange={handleDateFilter}/>
             </div>{/* filter */}
             <div className="grid grid-cols-4 gap-8 justify-items-center">
                         {events.length === 0 ?
